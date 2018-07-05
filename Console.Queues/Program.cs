2c0e3aa0afac940c4a63776925511966e14ecd88
rsantosdev <rsantos@braziliandevs.com>
2018-07-05 08:24:31 -0300

    Blobs and Queues Samples

---
+++ Console.Queues/Program.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Console.Queues
+{
+    class Program
+    {
+        private const string name = "envia-email";
+
+        static async Task Main(string[] args)
+        {
+            // Criar o objeto da conta
+            var storageAccount = CloudStorageAccount.Parse("suaConnectionString");
+
+            // Criar QueueClient
+            var queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Envia uma mensagem pra fila
+            await EnviaMensagemJson(queueClient);
+            //await EnviaMensagem(queueClient);
+
+            // Recupera numero de mensagens na fila
+            await ContaMensagens(queueClient);
+
+            // Checar mensagens sem marcalas como lida
+            //await ChecaMensagens(queueClient);
+
+            // Processar as mensagens
+            await ProcessaMensagensJson(queueClient);
+            //await ProcessaMensagens(queueClient);
+
+            // Recupera numero de mensagens na fila
+            await ContaMensagens(queueClient);
+        }
+
+        private static async Task ChecaMensagens(CloudQueueClient queueClient)
+        {
+            var queue = queueClient.GetQueueReference(name);
+
+            var messsages = await queue.PeekMessagesAsync(10);
+            foreach (var msg in messsages)
+            {
+                // Envio de Email
+                var content = msg.AsString;
+                System.Console.WriteLine($"Enviando email do cliente: {content}");
+            }
+        }
+
+        private static async Task ProcessaMensagens(CloudQueueClient queueClient)
+        {
+            var queue = queueClient.GetQueueReference(name);
+
+            var messsages = await queue.GetMessagesAsync(10);
+            //var messsages = await queue.GetMessagesAsync(10, TimeSpan.FromSeconds(5), new QueueRequestOptions(), new OperationContext());
+            foreach (var msg in messsages)
+            {
+                // Informações da Mensagem
+                System.Console.WriteLine($"Próxima leitura disponivel: {msg.NextVisibleTime.Value.ToLocalTime()}");
+
+                // Envio de Email
+                var content = msg.AsString;
+                System.Console.WriteLine($"Enviando email do cliente: {content}");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                System.Console.WriteLine($"Mensagem enviada!");
+
+                // Apaga mensagem da Queue
+                // msg.DequeueCount -> Quantas vezes essa mensagem ja foi processada
+                await queue.DeleteMessageAsync(msg);
+            }
+        }
+
+        private static async Task ProcessaMensagensJson(CloudQueueClient queueClient)
+        {
+            var queue = queueClient.GetQueueReference(name);
+
+            var messsages = await queue.GetMessagesAsync(10);
+            //var messsages = await queue.GetMessagesAsync(10, TimeSpan.FromSeconds(5), new QueueRequestOptions(), new OperationContext());
+            foreach (var msg in messsages)
+            {
+                // Informações da Mensagem
+                System.Console.WriteLine($"Próxima leitura disponivel: {msg.NextVisibleTime.Value.ToLocalTime()}");
+
+                // Envio de Email
+                var emailModel = JsonConvert.DeserializeObject<EmailModel>(msg.AsString);
+                System.Console.WriteLine($"Enviando email do cliente: {emailModel.UserId} / {emailModel.Email}");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                System.Console.WriteLine($"Mensagem enviada!");
+
+                // Apaga mensagem da Queue
+                // msg.DequeueCount -> Quantas vezes essa mensagem ja foi processada
+                await queue.DeleteMessageAsync(msg);
+            }
+        }
+
+        private static async Task ContaMensagens(CloudQueueClient queueClient)
+        {
+            var queue = queueClient.GetQueueReference(name);
+            await queue.FetchAttributesAsync();
+            System.Console.WriteLine($"Mensagens na fila: {queue.ApproximateMessageCount}");
+        }
+
+        static async Task EnviaMensagem(CloudQueueClient queueClient)
+        {
+            var queue = queueClient.GetQueueReference(name);
+            await queue.CreateIfNotExistsAsync();
+
+            var rnd = new Random();
+            var message = new CloudQueueMessage($"clientId-{rnd.Next(100)}");
+            await queue.AddMessageAsync(message);
+            System.Console.WriteLine($"Mensagem enviada para fila: {message.Id}");
+        }
+
+        static async Task EnviaMensagemJson(CloudQueueClient queueClient)
+        {
+            var queue = queueClient.GetQueueReference(name);
+            await queue.CreateIfNotExistsAsync();
+
+            var emailModel = new EmailModel { UserId = 55, Email = "rsantos@braziliandevs.com" };
+            var message = new CloudQueueMessage(JsonConvert.SerializeObject(emailModel));
+            await queue.AddMessageAsync(message);
+            System.Console.WriteLine($"Mensagem enviada para fila: {message.Id}");
+        }
+    }
+}
--- Console.Storage/Program.cs
+++ Console.Storage/Program.cs
@@ -1,12 +1,96 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Threading.Tasks;
 
 namespace Console.Storage
 {
     class Program
     {
-        static void Main(string[] args)
+        const string name = "pastacurso";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Criar o objeto da conta
+            var storageAccount = CloudStorageAccount.Parse("suaConnectionString");
+
+            // Criar BlobClient
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Listar Containers
+            await ListaContainers(blobClient);
+
+            // Criar Container
+            await CriaContainer(blobClient);
+
+            // Criar Blob e Fazer Upload
+            await CriaBlob(blobClient);
+
+            // Fazer Download do blob
+            await DownloadBlob(blobClient);
+
+            // Gerar SAS Token para acesso publico
+            CriaSasToken(blobClient);
+        }
+
+        static void CriaSasToken(CloudBlobClient blobClient)
+        {
+            var container = blobClient.GetContainerReference(name);
+            var blob = container.GetBlockBlobReference("curso.txt");
+
+            var sasToken = blob.GetSharedAccessSignature(new SharedAccessBlobPolicy
+            {
+                SharedAccessStartTime = DateTime.Today.AddDays(-1),
+                SharedAccessExpiryTime = DateTime.Today.AddDays(+1),
+                Permissions = SharedAccessBlobPermissions.Read
+            });
+
+            System.Console.WriteLine($"Sas Token: {sasToken}");
+        }
+
+        static async Task DownloadBlob(CloudBlobClient blobClient)
+        {
+            var container = blobClient.GetContainerReference(name);
+            var blob = container.GetBlockBlobReference("curso.txt");
+
+            var content = await blob.DownloadTextAsync();
+            System.Console.WriteLine($"Conteudo: {content}");
+            System.Console.WriteLine($"Tipo Conteudo: {blob.Properties.ContentType}");
+        }
+
+        static async Task CriaBlob(CloudBlobClient blobClient)
+        {
+            var container = blobClient.GetContainerReference(name);
+            var blob = container.GetBlockBlobReference("curso.txt");
+
+            var exists = await blob.ExistsAsync();
+            if (!exists)
+            {
+                await blob.UploadTextAsync("Hello blobs on Azure!");
+            }
+            //else
+            //{
+            //    blob.Properties.ContentType = "text/plain";
+            //    await blob.SetPropertiesAsync();
+            //}
+
+            System.Console.WriteLine($"Url para download: {blob.Uri}");
+        }
+
+        static async Task CriaContainer(CloudBlobClient blobClient)
+        {
+            var container = blobClient.GetContainerReference(name);
+            await container.CreateIfNotExistsAsync();
+            System.Console.WriteLine($"Container {name} created!");
+        }
+
+        static async Task ListaContainers(CloudBlobClient blobClient)
+        {
+            var containers = await blobClient.ListContainersSegmentedAsync(null);
+            foreach (var c in containers.Results)
+            {
+                System.Console.WriteLine(c.Name);
+            }
         }
     }
 }
